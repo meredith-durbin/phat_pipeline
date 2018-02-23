85013cdffd9acb2cdc98595d16a44c01163f9841
Meredith Durbin <meredith.durbin@gmail.com>
2018-02-23 13:39:47 -0800

    start documenting make_cmds

--- scripts/make_cmds.py
+++ scripts/make_cmds.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-"""Makes CMDs of ST and GST measurements.
+"""Makes CMDs of GST measurements.
 
 Authors
 -------
@@ -14,48 +14,69 @@ Use
         python make_cmds.py ['filebase']
     
     Parameters:
-    (Required) [filebase] - Path to .phot file, with or without .phot extension.
-    (Optional) [--to_hdf] - Whether to write the dataframe to an HDF5 
-    file. Default is True.
-    (Optional) [--full] - Whether to use the full set of columns (photometry of 
-    individual exposures). Default is False.
+    (Required) [filebase] - Path to .phot.hdf5 file
 """
-
+import matplotlib
+matplotlib.use('Agg') # check this
 import matplotlib.pyplot as plt
 import numpy as np
 import vaex
-
 try:
     import seaborn as sns; sns.set(style='white', font_scale=1.3)
 except ImportError:
     print('install seaborn you monster')
 
 # need better way to do this
-try:
-    ds = vaex.open('testing/14610_M33-B01-F01.phot.hdf5', key='data')
-except:
-    import pandas as pd
-    df = pd.read_hdf('testing/14610_M33-B01-F01.phot.hdf5', key='data')
-    df['gst_count'] = df.filter(regex='gst').sum(axis=1).astype(int)
-    ds = vaex.from_pandas(df)
 
 def make_cmd(ds, red_filter, blue_filter, y_filter, n_err=12,
              density_kwargs={'f':'log10', 'colormap':'viridis'},
              scatter_kwargs={'c':'k', 'alpha':0.5, 's':1}):
+    """Plot a CMD with (blue_filter - red_filter) on the x-axis and 
+    y_filter on the y-axis.
+
+    Inputs
+    ------
+    ds : Dataset
+        Vaex dataset
+    red_filter : string
+        filter name for "red" filter
+    blue_filter : string
+        filter name for "blue" filter
+    y_filter : string
+        filter name for filter on y-axis (usually same as red_filter)
+    n_err : int, optional
+        number of bins to calculate median photometric errors for
+        default: 12
+    density_kwargs : dict, optional
+        parameters to pass to ds.plot; see vaex documentation
+    scatter_kwargs : dict, optional
+        parameters to pass to ds.scatter; see vaex documentation
+
+    Returns
+    -------
+    Nothing
+
+    Outputs
+    -------
+    some plots dude
+    """
     color = '{}_{}_vega'.format(blue_filter, red_filter)
     blue_vega = '{}_vega'.format(blue_filter)
     red_vega = '{}_vega'.format(red_filter)
     y_vega = '{}_vega'.format(y_filter)
     ds[color] = ds[blue_vega]-ds[red_vega]
     gst_criteria = ds['{}_gst'.format(red_filter)] & ds['{}_gst'.format(blue_filter)]
-    if (y_filter != blue_filter) & (y_filter != red_filter):
+    if y_filter not in [blue_filter, red_filter]:
+        # idk why you would do this but it's an option
         gst_criteria = gst_criteria & ds['{}_gst'.format(y_filter)]
+    # cut dataset down to gst stars
     ds_gst = ds[gst_criteria]
+    # haxx
     xmin = np.nanmin(ds_gst[color].tolist())
     xmax = np.nanmax(ds_gst[color].tolist())
     ymin = np.nanmin(ds_gst[y_vega].tolist())
     ymax = np.nanmax(ds_gst[y_vega].tolist())
-    if ds_gst.length() > 50000:
+    if ds_gst.length() >= 50000:
         fig, ax = plt.subplots(1, figsize=(6.,4.))
         data_shape = int(np.sqrt(ds_gst.length()))
         ds_gst.plot(color, y_vega, shape=data_shape,
@@ -73,3 +94,26 @@ def make_cmd(ds, red_filter, blue_filter, y_filter, n_err=12,
     x_binned = [xmax*0.9]*n_err
     ax.errorbar(x_binned, y_binned, yerr=yerr, xerr=xerr,
                 fmt=',', color='k', lw=1.5)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filebase', action='store')
+    args = parser.parse_args()
+
+    photfile = args.filebase
+
+    try:
+        # I have never gotten vaex to read an hdf5 file successfully
+        ds = vaex.open(photfile)
+    except:
+        import pandas as pd
+        df = pd.read_hdf(photfile, key='data')
+        ds = vaex.from_pandas(df)
+
+    # how choose filters WHOM KNOWS
+    make_cmd(ds, red_filter, blue_filter, y_filter)
+
+
+
